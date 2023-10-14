a711c3bf650d83e7aae9404e7f65604a999511d9
bashkirian <exaggeratethismotion@gmail.com>
2023-10-14 05:21:46 +0300

    fix errors

--- backend/controllers/bank_branches.go
+++ backend/controllers/bank_branches.go
@@ -15,17 +15,20 @@ func FindBanks(c *gin.Context) error {
 	var banks []*models.Bank
 	rows, err := models.DB.Query("SELECT id, salepointname, latitude, longitude FROM banks;") 
 	if err != nil {
-		return fmt.Errorf("select: %w", err)
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return fmt.Errorf("%w", err)
 	}
 	for rows.Next() {
 		b := new(models.Bank)
 		err = rows.Scan(&b.ID, &b.Name, &b.Latitude, &b.Longitude)
 		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return fmt.Errorf("rows scan: %w", err)
 		}
 		banks = append(banks, b)
 	}
 	if rows.Err() != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return fmt.Errorf("rows: %w", err)
 	}
 	c.JSON(http.StatusOK, gin.H{"data": banks})
@@ -40,16 +43,19 @@ func FindBank(c *gin.Context) error {
 	selectStatement := `SELECT id, salepointname, latitude, longitude FROM banks WHERE id = $1`
 	rows, err := models.DB.Query(selectStatement, c.Param("id")) 
 	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 		return fmt.Errorf("select bank: %w", err)
 	}
 	for rows.Next() {
 		b := new(models.Bank)
 		err = rows.Scan(&b.ID, &b.Name, &b.Latitude, &b.Longitude)
 		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return fmt.Errorf("scan bank: %w", err)
 		}
 		bank = b
 		if rows.Err() != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return fmt.Errorf("rows: %w", err)
 		}
 	}
